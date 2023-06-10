8cb21f54396582887fefcf919da9df2dfa51710f
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-10 10:22:20 +0900

    Add: ノートを追加する機能を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import "./App.css";
+import { useState } from "react";
+import uuid from "react-uuid";
+import "./styles/App.css";
 import Main from "./components/Main";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  const onAddNote = () => {
+    const newNote = {
+      id: uuid(),
+      title: "ノートのタイトル",
+      content: "ノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  };
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} notes={notes} />
       <Main />
     </div>
   );
