75c3adc06915f62fcbd5a8077d611bb509c6f557
tsutsui kazunari <bakatesuto.samon1997@gmail.com>
2023-06-10 10:29:02 +0900

    Add: ノートの削除機能を追加

--- src/App.jsx
+++ src/App.jsx
@@ -18,9 +18,18 @@ function App() {
     console.log(notes);
   };
 
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  };
+
   return (
     <div className="App">
-      <Sidebar onAddNote={onAddNote} notes={notes} />
+      <Sidebar
+        onAddNote={onAddNote}
+        onDeleteNote={onDeleteNote}
+        notes={notes}
+      />
       <Main />
     </div>
   );
